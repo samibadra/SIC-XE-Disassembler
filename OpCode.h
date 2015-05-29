c4722b35709c620d2a8c2e4f5a023bc73f4fbe9b
Sami Badra <samibadra@yahoo.com>
2015-05-29 05:04:24 -0700

    Update OpCode.h

--- OpCode.h
+++ OpCode.h
@@ -6,10 +6,6 @@
  FILE: OpCode.h
  
  Copyright (c) 2015 Sami Badra. All rights reserved.
- 
- DISCLAIMER: Any unauthorized use, including but not limited to, copying or
- redistributing any chunk of the source code (or an entire file) will result in
- punishment by law. I, Sami Badra, own all rights to the files and their contents.
  */
 
 #ifndef __a2__C_____OpCode__
